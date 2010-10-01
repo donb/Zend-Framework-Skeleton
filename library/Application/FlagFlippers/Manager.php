d437da182a07baa20132349bc9897a34aacb0d42
christophervalles <info@christophervalles.com>
2010-10-01 21:53:49 +0200

    Flag and flippers

--- library/Application/FlagFlippers/Manager.php
+++ library/Application/FlagFlippers/Manager.php
@@ -55,7 +55,7 @@ class Application_FlagFlippers_Manager
     }
     
     public function isAllowed($role, $resource){
-        return Application_FlagFlippers_Manager::_getFromRegistry()->isAllowed('christopher', 'index');
+        return Application_FlagFlippers_Manager::_getFromRegistry()->isAllowed($role, $resource);
     }
     
     /**
